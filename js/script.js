a0aa2456ac81dadae677d00460430b832914565d
Maksatka <97061097+Maksatka@users.noreply.github.com>
2023-01-26 14:38:51 +0400

    two test

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
+};
+
+var swiper = new Swiper(".home-slider", {
+   loop:true,
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+});
+
+/***** Swiper Slider scripts init here *****/
+var swiper = new Swiper(".home-slider", {
+	spaceBetween: 0,
+	centeredSlides: true,
+	loop: true,
+	autoplay: {
+	  delay: 5000,
+	  disableOnInteraction: false,
+	},
+	pagination: {
+	  el: ".swiper-pagination",
+	  clickable: true,
+	},
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+  });
+
